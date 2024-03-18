30075bef0fe5905988d5dcef3ef685044ddae4a0
Deepak Nautiyal <deepaknautiyal@Deepaks-MacBook-Pro.local>
2024-03-18 13:53:55 +0530

    setting healthcheck endpoint on port=3000

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -2,18 +2,7 @@ package healthcheck
 
 import "net/http"
 
-func Healthcheck() string {
-
-	http.HandleFunc("/alive", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Application is alive and healthy\n"))
-	})
-
-	// Start the HTTP server on port 8080 in a separate goroutine
-	go func() {
-		if err := http.ListenAndServe(":8080", nil); err != nil {
-			panic(err)
-		}
-	}()
-	return "health endpoint enabled [ port=8080, path=/alive ]"
+func Healthcheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Application is alive and healthy\n"))
 }
